de2980b13b5a5b7972a3769c954d696a930b7f48
jonghun <jonghuncu@gmail.com>
2023-05-09 13:08:46 +0900

    feat : Member 관련 클래스

---
+++ src/main/java/com/team/synergy/member/Member.java
@@ -0,0 +1,30 @@
+package com.team.synergy.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Getter
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    private String password;
+
+    private String email;
+
+    private LocalDateTime createDate;
+}
---
+++ src/main/java/com/team/synergy/member/MemberDto.java
@@ -0,0 +1,10 @@
+package com.team.synergy.member;
+
+import lombok.Getter;
+
+@Getter
+public class MemberDto {
+    private String name;
+    private String email;
+    private String password;
+}
