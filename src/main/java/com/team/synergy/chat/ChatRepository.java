4a4e13b192920f58dd9c50de469dac0f7f5c38f8
jonghun <jonghuncu@gmail.com>
2023-07-16 20:47:07 +0900

    feat : 채팅 기능

---
+++ src/main/java/com/team/synergy/chat/ChatRepository.java
@@ -0,0 +1,32 @@
+package com.team.synergy.chat;
+
+import org.springframework.stereotype.Repository;
+
+import javax.annotation.PostConstruct;
+import java.util.*;
+
+@Repository
+public class ChatRepository {
+    private Map<String, ChatRoom> chatRoomMap;
+
+    @PostConstruct
+    private void init() {
+        chatRoomMap = new LinkedHashMap<>();
+    }
+
+    public List<ChatRoom> findAllRoom() {
+        List<ChatRoom> chatRooms = new ArrayList<>(chatRoomMap.values());
+        Collections.reverse(chatRooms);
+        return chatRooms;
+    }
+
+    public ChatRoom findRoomById(String id) {
+        return chatRoomMap.get(id);
+    }
+
+    public ChatRoom createChatRoom(String name) {
+        ChatRoom chatRoom = ChatRoom.create(name);
+        chatRoomMap.put(chatRoom.getRoomId(), chatRoom);
+        return chatRoom;
+    }
+}
