76f58ecafe7aeb37dd5bcf3370af604b41124da8
jonghun <jonghuncu@gmail.com>
2023-09-29 03:59:22 +0900

    update gradle.yml 배포 자동화

--- src/main/java/com/team/synergy/SynergyApplication.java
+++ src/main/java/com/team/synergy/SynergyApplication.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SynergyApplication {
 
-    @GetMapping("/h")
+    @GetMapping("/hello")
     public String hello() {
         return "now 2023 09 29:02:28";
     }
